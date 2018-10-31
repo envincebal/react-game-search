11c223990ad316adf136473b44c184428e9d797b
envincebal <vincentyan1@gmail.com>
2018-10-30 20:42:34 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import * as GameAPI from "../src/utils/GameAPI";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/utils/GameAPI.js
@@ -0,0 +1,12 @@
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const key = "8cd10a7136710c1003c8e216d85941ace5a1f00e";
+
+export const getGames = (searchGame) => {
+  const endpoint = `https://www.giantbomb.com/api/search/?api_key=`;
+  const url = proxyUrl + endpoint + key + `&format=json&query=${searchGame}&limit=30`;
+
+  return fetch(url)
+  .then(res => res.json())
+  .then(res2 => res2); 
+}
+
