7b422df9e47d9dacc7303fdfaa436d7f8d865802
envincebal <vincentyan1@gmail.com>
2018-11-12 10:36:11 -0800

    Updates

---
+++ precache-manifest.2980305c094e6eb94c3b49226030ab9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a54017e17583d84c27e4c020258bb29",
+    "url": "/react-game-search/static/media/consoles.8a54017e.jpg"
+  },
+  {
+    "revision": "055dcedcb6a649b696c2",
+    "url": "/react-game-search/static/js/runtime~main.055dcedc.js"
+  },
+  {
+    "revision": "33366f390337a4c179cd",
+    "url": "/react-game-search/static/js/main.33366f39.chunk.js"
+  },
+  {
+    "revision": "2e332e7c0fb444f8a887",
+    "url": "/react-game-search/static/js/1.2e332e7c.chunk.js"
+  },
+  {
+    "revision": "33366f390337a4c179cd",
+    "url": "/react-game-search/static/css/main.b9bed57b.chunk.css"
+  },
+  {
+    "revision": "1fc590aa8e8dd060e8ff52dceeb12796",
+    "url": "/react-game-search/index.html"
+  }
+];
