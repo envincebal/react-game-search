26c5168ba50062085902f1041bc5eb617f1a9d14
envincebal <vincentyan1@gmail.com>
2018-11-12 15:24:23 -0800

    Changed About list to a paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,11 @@ const About = (props) => {
   return (
     <div className="about-container">
       <h1 className="about-title">About</h1>
-      <ul className="about-list">
-        <li>The API provided for this app came from the good people at <a className="giant-bomb-link" href="https://www.giantbomb.com/api/">GiantBomb</a>.</li>
-        <li>This was created for all the wonderful lovers of video games out there.</li>
-        <li><strong>DISCLAIMER:</strong> I am not responsible for anyone ending up in a World of Warcraft addiction program.</li>
-        <li>I built this app with plenty of love and countless liters of Dr. Pepper.</li>
-      </ul>
+      <p className="about-statement">
+        The API provided for this app came from the good people at <a className="giant-bomb-link" rel="noopener noreferrer"
+        target="_blank" href="https://www.giantbomb.com/api/">GiantBomb</a>.
+       This was created for all the wonderful lovers of video games out there. <strong>DISCLAIMER:</strong> I am not responsible for anyone ending up in a World of Warcraft addiction program. I built this app with plenty of love and countless liters of Dr. Pepper.
+      </p>
     </div>
   );
 }
