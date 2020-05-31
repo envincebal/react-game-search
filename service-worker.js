3063f4e5aaf644872a72b87be596ef56ca236653
envincebal <vincentyan1@gmail.com>
2020-05-31 02:33:07 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-game-search/main/precache-manifest.21d850ed5b95ca511522772e54074718.js"
+  "/react-game-search/precache-manifest.ea2a58d56e15ae455e514f977242484d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-game-search/main/index.html", {
+workbox.routing.registerNavigationRoute("/react-game-search/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
