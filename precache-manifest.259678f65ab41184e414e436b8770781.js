e72feb51874cce7c662d231178c1927367a91132
envincebal <vincentyan1@gmail.com>
2020-08-24 19:41:51 -0700

    Updates

---
+++ precache-manifest.259678f65ab41184e414e436b8770781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a1c6eaf33e11e7078f1b2ba1b1a70d",
+    "url": "/react-game-search/index.html"
+  },
+  {
+    "revision": "47f7b06097fa49e5f1b7",
+    "url": "/react-game-search/static/css/main.bede2b14.chunk.css"
+  },
+  {
+    "revision": "13e34ccab3359da1d297",
+    "url": "/react-game-search/static/js/2.d641405a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game-search/static/js/2.d641405a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f7b06097fa49e5f1b7",
+    "url": "/react-game-search/static/js/main.640bb9f7.chunk.js"
+  },
+  {
+    "revision": "e03166b2dea231f429ee",
+    "url": "/react-game-search/static/js/runtime-main.7e8ed6b4.js"
+  },
+  {
+    "revision": "8a54017e17583d84c27e4c020258bb29",
+    "url": "/react-game-search/static/media/consoles.8a54017e.jpg"
+  },
+  {
+    "revision": "7f0b5126ffaf042c6a379705f091b62d",
+    "url": "/react-game-search/static/media/no-box.7f0b5126.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-game-search/precache-manifest.bd6db0eb93779aed1bd8f047a13366fe.js"
+  "/react-game-search/precache-manifest.259678f65ab41184e414e436b8770781.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-game-search/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-game-search/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
