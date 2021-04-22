73dd47cfc97e7bda650b1932ad9bc73568af59fa
envincebal <vincentyan1@gmail.com>
2021-04-22 02:06:04 -0700

    Added CORS Proxy.

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Game from './Game';
 
 const GamesList = (props) => {
-  return props.gamesList.map((game, id) => {
+  return props.gamesList.map((game) => {
     return <Game
       key={game.id}
       game={game.name}
